a11dcf4479247daf5900afae94dec16d9283e8ac
Kam Qasim <kamqasim1@gmail.com>
2020-07-13 23:12:52 +0400

    Inittial Commit

---
+++ EI-IA/EI-IA/EI_IA.h
@@ -0,0 +1,19 @@
+//
+//  EI_IA.h
+//  EI-IA
+//
+//  Created by muhammad on 7/13/20.
+//  Copyright © 2020 Insurance Authority. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EI_IA.
+FOUNDATION_EXPORT double EI_IAVersionNumber;
+
+//! Project version string for EI_IA.
+FOUNDATION_EXPORT const unsigned char EI_IAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EI_IA/PublicHeader.h>
+
+
